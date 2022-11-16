2cb9e01843194ec3b7a9a8af4a0bb11ffd94974f
amir <amir.developer8@gmail.com>
2022-11-17 01:43:49 +0330

    hashed password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -10,14 +12,12 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registerd!");
 
-  user = new User({
-    email: req.body.email,
-    name: req.body.name,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
