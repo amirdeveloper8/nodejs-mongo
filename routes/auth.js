0a6f69587206fc304ad25d8a42155d093a32a5ae
amir <amir.developer8@gmail.com>
2022-11-17 03:26:41 +0330

    add auth with token

---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Inavlid email or Password!");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Inavlid email or Password!");
+
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res.send(token);
+});
+
+const validate = (body) => {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+
+  const result = schema.validate(body);
+  return result;
+};
+
+module.exports = router;
